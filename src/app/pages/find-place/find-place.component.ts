24cfa081b786155e1794863f680825e578cc20b5
adriano.moda <adrianomodafeitosa@gmail.com>
2021-07-29 10:28:44 -0300

    layout mapa e form

--- src/app/pages/find-place/find-place.component.ts
+++ src/app/pages/find-place/find-place.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { } from "google.maps";
 
 @Component({
@@ -19,6 +20,10 @@ export class FindPlaceComponent implements OnInit {
       center: { lat: -34.397, lng: 150.644 },
       zoom: 8
     })
+  }
+
+  enviarLocalizacao = (f: NgForm) => {
+    alert(f.value.latitude)
   }
 
 }
