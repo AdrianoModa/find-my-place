bdd955de14deaeb85c795986d6053297108656ef
adriano.moda <adrianomodafeitosa@gmail.com>
2021-07-29 07:36:08 -0300

    renderizando mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { } from "google.maps";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'find-my-place';
+export class AppComponent implements OnInit {
+  
+  map: google.maps.Map
+  
+  ngOnInit(): void {
+    this.initMap()
+  }
+
+
+initMap(): void {
+  const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
 }
